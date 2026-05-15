8376b96c2c8bdac09a45262c8095e0674e863447
mkmcgrath <matthew.k.mcgrath3@gmail.com>
2026-05-15 11:09:15 -0600

    change swagger host to remote

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'Contacts API',
     description: 'Contacts API for CSE341',
   },
-  host: 'localhost:3000',
+  host: 'cse341-squirm.onrender.com',
   schemes: ['http'],
   definitions: {
     Contact: {
@@ -20,9 +20,5 @@ const doc = {
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
-
-/* NOTE: if you use the express Router, you must pass in the 
-   'endpointsFiles' only the root file where the route starts,
-   such as index.js, app.js, routes.js, ... */
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
