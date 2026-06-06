1b0e8bd88bae96f691d5da764c40272efd95a171
mkmcgrath <matthew.k.mcgrath3@gmail.com>
2026-06-06 12:40:21 -0600

    get started implementing oauth

--- 2/routes/index.js
+++ 2/routes/index.js
@@ -1,9 +1,33 @@
 const router = require('express').Router();
+const passport = require('passport');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+router.get('/login', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
+
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/api-docs', session: true }),
+  (req, res) => {
+    res.redirect('/');
+  }
+);
+
+router.get('/', (req, res) => {
+  res.send(req.user !== undefined ? `Logged in as ${req.user.displayName}` : 'Logged Out');
+});
 
 router.use('/flashcards', require('./flashcards'));
 
